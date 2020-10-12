07f7ff5858981a996f0dfb1e01f4a04b72a14ef7
Simon Panek <paneks19@gmail.com>
2020-10-12 16:22:15 -0700

    random images and vote count working

--- js/app.js
+++ js/app.js
@@ -3,18 +3,28 @@
 // global variables ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // needed: array of objects, voting rounds
 
-allProducts = [];
-
 var votingRounds = 25; //change this value to control the number of rounds to be run, 25 is default
+var votingRoundTracker = 1; //keeps track of each round of voting, starts at 1 because the initial vote is populated at page load
+
+var allProducts = [];
+
+var imageOneElement = document.getElementById('imgOne');
+var imageTwoElement = document.getElementById('imgTwo');
+var imageThreeElement = document.getElementById('imgThree');
+var imageSection = document.getElementById('imgSection');
+
+var randomNumberTracker = [];
+var randomCombinationTracker =[];
 
 
 // constructor function with name of product and filePath parameters//////////////////////////////////////////////////////
 
-function Product(productName, imgFilePath){
+function Product(productName, imgFilePath, imageNumber){
 
   this.productName = productName;
-  this.ingFilePath = imgFilePath;
-  
+  this.imgFilePath = imgFilePath;
+  this.imageNumber = imageNumber;
+
   this.productVoteCount = 0;
   this.productDisplayCount = 0;
 
@@ -23,50 +33,89 @@ function Product(productName, imgFilePath){
 
 console.log(allProducts);
 
-//prototypes//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//random picture render
-
 //instances//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-new Product('bag', 'img/ImageBitmapRenderingContext.jpg');
-new Product('banana', 'img/banana.jpg');
-new Product('bathroom', 'img/bathroom/jpg');
-new Product('boots', 'img/boots.jpg');
-new Product('breakfast.jpg', 'img/breakfast.jpg');
-new Product('bubblegum', 'img/bubblegum.jpg');
-new Product('chair', 'img/chair.jpg');
-new Product('cthulhu', 'img/cthulhu');
-new Product('dog-duck', 'img/dog-duck.jpg');
-new Product('dragon', 'img/dragon.jpg');
-new Product('pen', 'img/pen.jpg');
-new Product('pet-sweep', 'img/petsweep.jpg');
-new Product('scissors', 'img/scissors.jpg');
-new Product('shark', 'img/shark.jpg');
-new Product('sweep', 'img/sweep.png');
-new Product('tauntaun', 'img/tauntaun.jpg');
-new Product('unicorn', 'img/unicorn.jpg');
-new Product('usb', 'img/usb.gif');
-new Product('water-can', 'img/water-can.jpg');
-new Product('wine-glass', 'img/wine-glass.jpg');
+new Product('bag', 'img/bag.jpg', 1);
+new Product('banana', 'img/banana.jpg', 2);
+new Product('bathroom', 'img/bathroom.jpg', 3);
+new Product('boots', 'img/boots.jpg', 4);
+new Product('breakfast.jpg', 'img/breakfast.jpg', 5);
+new Product('bubblegum', 'img/bubblegum.jpg', 6);
+new Product('chair', 'img/chair.jpg', 7);
+new Product('cthulhu', 'img/cthulhu.jpg', 8);
+new Product('dog-duck', 'img/dog-duck.jpg', 9);
+new Product('dragon', 'img/dragon.jpg', 10);
+new Product('pen', 'img/pen.jpg', 11);
+new Product('pet-sweep', 'img/pet-sweep.jpg', 12);
+new Product('scissors', 'img/scissors.jpg', 13);
+new Product('shark', 'img/shark.jpg', 14);
+new Product('sweep', 'img/sweep.png', 15);
+new Product('tauntaun', 'img/tauntaun.jpg', 16);
+new Product('unicorn', 'img/unicorn.jpg', 17);
+new Product('usb', 'img/usb.gif', 18);
+new Product('water-can', 'img/water-can.jpg', 19);
+new Product('wine-glass', 'img/wine-glass.jpg', 20);
 
+//prototypes//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//random picture render
+function render(imageElement){
+  var randomIndexPosition = randomNumberGenerator (0, allProducts.length-1);
 
+  while(randomNumberTracker.includes(randomIndexPosition)){
+    randomIndexPosition = randomNumberGenerator (0, allProducts.length-1);
+  }
 
+  imageElement.src = allProducts[randomIndexPosition].imgFilePath;
+  imageElement.alt = allProducts[randomIndexPosition].productName;
 
+  Product.productDisplayCount ++;
 
+  for (var i=2; i<randomNumberTracker.length; i++){ //clear random number variables beyond index position 1
+    randomNumberTracker.pop();
+  }
+
+  randomNumberTracker.push(randomIndexPosition);
+
+}
 
 
 // helper functions/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // random number generator
 
-
+function randomNumberGenerator(min, max){
+  return Math.floor(Math.random() * (max - min +1) + min);
+}
 
 //event listener: on 'click' generate three new products, increase vote count, increase shown count////////////////////
 // remove listener after reaching the target number of rounds
 
+imageSection.addEventListener('click', function(event){ // listens for click then runs function
+  if(votingRoundTracker < votingRounds+1){
+    render(imageOneElement);
+    render(imageTwoElement);
+    render(imageThreeElement);
 
+    var chosenProduct = event.target.alt; //declares variable to store which picture was clicked on to increase appropriate vote count
+
+    for (var i=0; i<allProducts.length; i++){
+      if(chosenProduct === allProducts[i].productName){
+        allProducts[i].productVoteCount++;
+      }
+    }
+
+    votingRoundTracker++;
+
+  } else {
+    // this is where to output final scores
+  }
+});
+
+console.log('after listener', allProducts);
 
 //executables//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+render(imageOneElement);
+render(imageTwoElement);
+render(imageThreeElement);
